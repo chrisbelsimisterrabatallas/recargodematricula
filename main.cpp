6e2bff819da50b9775b0ae5c7ad33d13f5bb1b33
chrisbel.simisterra <chrisbel.simisterra@pucese.edu.ec>
2021-06-17 12:16:03 -0500

    Calcular el regargo de matricula

--- main.cpp
+++ main.cpp
@@ -4,8 +4,32 @@
 //y menor o igual que 15 año, el recargo sera del 2. Si la edad es mayor que
 //15 y menor o igual que 20, el recargo es del 4%. S ila edad es superior
 //a 20 años, el recargo es del 6%.
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int edad;
+    double recargo=0, costoMatricula=0;
+
+    cout<<"Ingresa la edad: ";
+    cin>>edad;
+    cout<<"Ingrese el costo de la matricula: ";
+
+    if (edad <5)
+        recargo=0;
+    else if (edad<=10) //(edad>=5 && edad<=10)
+        recargo=0.01;
+    else if (edad<=15)    // (edad>10 && edad<=15)
+        recargo=0.02;
+    else if (edad<=20)    // (edad>15 && edad<=20)
+        recargo=0.04;
+    else                  // (edad>20)
+        recargo=0.06;
+    double rec = costoMatricula * recargo;
+    double valorpagar= costoMatricula + rec;
+
+    cout<<"Tu edad es de: "<<edad <<"años, tu recargo es: "<<rec<<endl;
+    cout<<"Debes pagar en total: "<<valorpagar<<endl;
+
+
     return 0;
 }
